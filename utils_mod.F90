36e6dc6723fc9818b8bcfb2fec9ea55f691633f9
denise.worthen <Denise.Worthen@noaa.gov>
2023-10-12 13:40:00 -0400

    add ability to do ice post also
    * set logic flag for do_ocnpost
    * check for allocated arrays
    * use 2d mask for ice interpolation mask

--- utils_mod.F90
+++ utils_mod.F90
@@ -1,7 +1,7 @@
 module utils_mod
 
   use netcdf
-  use ocnvars, only : vardefs
+  use outputvars, only : vardefs
 
   implicit none
 
@@ -29,6 +29,7 @@ module utils_mod
   end interface getvecpair
 
   interface dumpnc
+     module procedure dumpnc1d
      module procedure dumpnc2d
      module procedure dumpnc3d
      module procedure dumpnc3dk
@@ -39,8 +40,8 @@ module utils_mod
   public remap
   public dumpnc
 
-  !logical :: debug = .true.
-  logical :: debug = .false.
+  logical :: debug = .true.
+  !logical :: debug = .false.
 
 contains
 
@@ -66,11 +67,11 @@ contains
     if (debug)print '(a)','enter '//trim(subname)
     ! obtain vector pairs
     do n = 1,nflds
-       if (trim(vars(n)%var_grid) == 'Cu') then
+       if (trim(vars(n)%var_grid) == 'Cu' .or. trim(vars(n)%var_grid) == 'Bu_x') then
           allocate(vecpair(dims(1)*dims(2),2)); vecpair = 0.0
-          call getvecpair(trim(filesrc), trim(wgtsdir), cosrot, sinrot, &
-               trim(vars(n)%input_var_name), trim(vars(n)%var_grid),    &
-               trim(vars(n)%var_pair), trim(vars(n)%var_pair_grid),     &
+          call getvecpair(trim(filesrc), trim(wgtsdir), cosrot, sinrot,   &
+               trim(vars(n)%input_var_name), trim(vars(n)%var_grid(1:2)), &
+               trim(vars(n)%var_pair), trim(vars(n)%var_pair_grid(1:2)),  &
                dims=(/dims(1),dims(2)/), vecpair=vecpair)
        end if
     end do
@@ -84,8 +85,12 @@ contains
                field=fields(:,nn))
        else ! fill with vector pairs
           nn = nn+1
+          ! ocn vectors
           if (trim(vars(n)%var_grid) == 'Cu')fields(:,nn) = vecpair(:,1)
           if (trim(vars(n)%var_grid) == 'Cv')fields(:,nn) = vecpair(:,2)
+          ! ice vectors
+          if (trim(vars(n)%var_grid) == 'Bu_x')fields(:,nn) = vecpair(:,1)
+          if (trim(vars(n)%var_grid) == 'Bu_y')fields(:,nn) = vecpair(:,2)
        end if
     end do
 
@@ -163,8 +168,10 @@ contains
 
     wgtsfile = trim(wdir)//'tripole.mx025.'//vgrid1//'.to.Ct.bilinear.nc'
     call getfield(fname, vname1, dims=dims, field=vecpair(:,1), wgts=trim(wgtsfile))
+    if (debug)print '(a)','wgtsfile for 2d vector '//trim(vname1)//'   '//trim(wgtsfile)
     wgtsfile = trim(wdir)//'tripole.mx025.'//vgrid2//'.to.Ct.bilinear.nc'
     call getfield(fname, vname2, dims=dims, field=vecpair(:,2), wgts=trim(wgtsfile))
+    if (debug)print '(a)','wgtsfile for 2d vector '//trim(vname2)//'   '//trim(wgtsfile)
 
     urot = 0.0; vrot = 0.0
     do ii = 1,dims(1)*dims(2)
@@ -365,7 +372,7 @@ contains
     integer :: i,ii,jj
     integer :: n_a, n_b, n_s
     integer(kind=4), allocatable, dimension(:) :: col, row
-    real(kind=8), allocatable, dimension(:)    :: S
+    real(kind=8),    allocatable, dimension(:) :: S
     character(len=20) :: subname = 'remap2d'
 
     if (debug)print '(a)','enter '//trim(subname)//' weights = '//trim(fname)
@@ -416,7 +423,7 @@ contains
     integer :: i,ii,jj
     integer :: n_a, n_b, n_s
     integer(kind=4), allocatable, dimension(:) :: col, row
-    real(kind=8), allocatable, dimension(:)    :: S
+    real(kind=8),    allocatable, dimension(:) :: S
     character(len=20) :: subname = 'remap3d'
 
     if (debug)print '(a)','enter '//trim(subname)//' weights = '//trim(fname)
@@ -463,7 +470,7 @@ contains
     real,             intent(in) :: field(:,:)
 
     ! local variable
-    integer           :: ncid, varid, rc, idimid, jdimid, fdimid
+    integer :: ncid, varid, rc, idimid, jdimid, fdimid
     real, allocatable :: a3d(:,:,:)
     character(len=20) :: subname = 'dumpnc2d'
 
@@ -495,7 +502,7 @@ contains
     real,             intent(in) :: field(:,:,:)
 
     ! local variable
-    integer           :: n, ncid, varid, rc, idimid, jdimid, kdimid, fdimid
+    integer :: n, ncid, varid, rc, idimid, jdimid, kdimid, fdimid
     real, allocatable :: a4d(:,:,:,:)
     character(len=20) :: subname = 'dumpnc3d'
 
@@ -511,7 +518,7 @@ contains
     rc = nf90_enddef(ncid)
 
     do n = 1,nflds
-       a4d(:,:,:,n) =  reshape(field(1:dims(1)*dims(2),1:dims(3),n), (/dims(1),dims(2),dims(3)/))
+       a4d(:,:,:,n) = reshape(field(1:dims(1)*dims(2),1:dims(3),n), (/dims(1),dims(2),dims(3)/))
     end do
     rc = nf90_put_var(ncid, varid, a4d)
     rc = nf90_close(ncid)
@@ -529,7 +536,7 @@ contains
     real,             intent(in) :: field(:,:)
 
     ! local variable
-    integer           :: n, ncid, varid, rc, idimid, jdimid, kdimid
+    integer :: n, ncid, varid, rc, idimid, jdimid, kdimid
     real, allocatable :: a3d(:,:,:)
     character(len=20) :: subname = 'dumpnc3dk'
 
@@ -552,11 +559,42 @@ contains
   end subroutine dumpnc3dk
 
   !----------------------------------------------------------
+  ! write a bare netcdf file of an unpacked 2D field
+  !----------------------------------------------------------
+  subroutine dumpnc1d(fname, vname, dims, field)
+
+    character(len=*), intent(in) :: fname, vname
+    integer,          intent(in) :: dims(:)
+    real,             intent(in) :: field(:)
+
+    ! local variable
+    integer           :: n, ncid, varid, rc, idimid, jdimid
+    real, allocatable :: a2d(:,:)
+    character(len=20) :: subname = 'dumpnc1d'
+
+    if (debug)print '(a)','enter '//trim(subname)//' variable '//vname
+    allocate(a2d(dims(1),dims(2))); a2d = 0.0
+
+    rc = nf90_create(trim(fname), nf90_clobber, ncid)
+    rc = nf90_def_dim(ncid, 'nx', dims(1), idimid)
+    rc = nf90_def_dim(ncid, 'ny', dims(2), jdimid)
+    rc = nf90_def_var(ncid, vname, nf90_float, (/idimid,jdimid/), varid)
+    rc = nf90_enddef(ncid)
+
+    a2d(:,:) =  reshape(field(1:dims(1)*dims(2)), (/dims(1),dims(2)/))
+    rc = nf90_put_var(ncid, varid, a2d)
+    rc = nf90_close(ncid)
+
+    if (debug)print '(a)','exit '//trim(subname)//' variable '//vname
+
+  end subroutine dumpnc1d
+
+  !----------------------------------------------------------
   ! handle netcdf errors
   !----------------------------------------------------------
   subroutine handle_err(ierr,string)
 
-    integer         , intent(in) :: ierr
+    integer ,         intent(in) :: ierr
     character(len=*), intent(in) :: string
     if (ierr /= nf90_noerr) then
       print '(a)', '*** ERROR ***: '//trim(string)//':'//trim(nf90_strerror(ierr))
